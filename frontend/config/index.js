34df1928da4c73297f4e0a74c4a491ddadbc986c
Lacragh <panterrosa90@gmail.com>
2025-04-26 12:12:18 -0500

    update index

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -29,7 +29,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/login': {
-        target: process.env.AUTH_API_ADDRESS || 'https://auth-api-111693207847.us-central1.run.app',
+        target: 'https://auth-api-111693207847.us-central1.run.app',
         secure: false
       },
       '/todos': {
